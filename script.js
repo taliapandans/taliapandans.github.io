50429aa93bab5360afe0ad84ec08d20ef6a6e96e
taliapandans <thaliapandans@gmail.com>
2021-04-09 20:12:34 +0200

    adding contact (right files)

--- script.js
+++ script.js
@@ -1,26 +1,77 @@
 $(document).ready(function(){
+
+    // sticky navbar on scroll script
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-button').click(function(){
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+        $('.menu-button i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["an electrical engineer", "a master's student", "available in the market"],
-        typeSpeed: 30,
+        typeSpeed: 50,
         backSpeed: 30,
         loop: true
     });
 
+    // skill progressbar
+    let options = {
+        startAngle : -1.55,
+        size: 150,
+        value: 0.85,
+        fill: {color: "red"}
+    }
+    $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+    function(event, progress, stepValue){
+        $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%")
+    });
+    $(".Moffice .bar").circleProgress({
+        value : 0.85, 
+    });
+    $(".MATLAB .bar").circleProgress({
+        value : 0.65, 
+    });
+    $(".SpaceEx .bar").circleProgress({
+        value : 0.60, 
+    });
+    $(".Eagle .bar").circleProgress({
+        value : 0.40, 
+    });
+    $(".Python .bar").circleProgress({
+        value : 0.30, 
+    });
+    $(".HTML .bar").circleProgress({
+        value : 0.20, 
+    });
+
+    // owl carousel script (projects)
+    $('.carousel').owlCarousel({
+        margin: 20,
+        // loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        // responsive: {
+        //     0:{
+        //         items: 1,
+        //         nav: false
+        //     },
+        //     600:{
+        //         items: 2,
+        //         nav: false
+        //     },
+        //     1000:{
+        //         items: 3,
+        //         nav: false
+        //     }
+        // }
+    });
 });
