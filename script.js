9e23760997e2e2e31443d56e6937f230860b4b12
taliapandans <thaliapandans@gmail.com>
2021-04-23 14:36:18 +0200

    correct background image

--- script.js
+++ script.js
@@ -59,19 +59,19 @@ $(document).ready(function(){
         // loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
-        // responsive: {
-        //     0:{
-        //         items: 1,
-        //         nav: false
-        //     },
-        //     600:{
-        //         items: 2,
-        //         nav: false
-        //     },
-        //     1000:{
-        //         items: 3,
-        //         nav: false
-        //     }
-        // }
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
     });
 });
