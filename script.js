fe02e9ab9f796e72c1f36c17d6256fcae6a0b75e
taliapandans <thaliapandans@gmail.com>
2021-09-13 21:10:44 +0200

    Adding job experience and skills

--- script.js
+++ script.js
@@ -55,8 +55,32 @@ $(document).ready(function(){
     $(".HTML .bar").circleProgress({
         value : 0.55, 
     });
+    $(".CV .bar").circleProgress({
+        value : 0.25, 
+    });
 
     // owl carousel script (projects)
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+    // owl carousel script (skills)
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
